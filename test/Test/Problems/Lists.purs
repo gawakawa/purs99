e298626b7ce601e0b7fea38918db07010d2d840b
gawakawa <mashurakiryu@icloud.com>
2025-10-16 20:41:13 +0900

    🎨 Format

--- test/Test/Problems/Lists.purs
+++ test/Test/Problems/Lists.purs
@@ -11,7 +11,7 @@ tests :: TestSuite
 tests = suite "Problems.Lists" do
   suite "myLast" do
     test "case 1" do
-      Assert.equal (Just 4) (myLast [1, 2, 3, 4])
-    
+      Assert.equal (Just 4) (myLast [ 1, 2, 3, 4 ])
+
     test "case 2" do
-      Assert.equal (Just 'z') (myLast ['x', 'y', 'z'])
+      Assert.equal (Just 'z') (myLast [ 'x', 'y', 'z' ])
